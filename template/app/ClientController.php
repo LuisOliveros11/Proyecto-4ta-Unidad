020078d662ecf274ff7919df29d245e084aae152
LuisOliveros11 <lparada_19@alu.uabcs.mx>
2024-11-18 11:20:37 -0700

    Implementación backend: Añadir y editar cliente

--- template/app/ClientController.php
+++ template/app/ClientController.php
@@ -2,23 +2,117 @@
 session_start();
 
 if (isset($_POST['addClient'])) {
-    $client_id = $_POST['client_id'];
-    $client_name = $_POST['client_name'];
-    $client_email = $_POST['client_email'];
-    $client_phone = $_POST['client_phone'];
-    $client_subscription = $_POST['client_subscription'];
-    $client_level = $_POST['client_level'];
+    if (
+        strlen($_POST['client_name']) > 0 &&
+        strlen($_POST['client_email']) > 0 &&
+        strlen($_POST['pwd']) > 0 &&
+        strlen($_POST['client_phone']) > 0 &&
+        strlen($_POST['client_subscription']) > 0 &&
+        strlen($_POST['client_level']) > 0
+    ) {
+        $client_name = $_POST['client_name'];
+        $client_email = $_POST['client_email'];
+        $pwd = $_POST['pwd'];
+        $client_phone = $_POST['client_phone'];
+        $client_subscription = $_POST['client_subscription'];
+        $client_level = $_POST['client_level'];
 
-    $clientController = new ClientController();
-    $clientController->crearCliente($client_name, $client_email, $client_phone, $client_subscription, $client_level);
+        $clientController = new ClientController();
+        $clientController->crearCliente($client_name, $client_email, $pwd, $client_phone, $client_subscription, $client_level);
+    } else {
+        echo "No se permiten campos vacios.";
+    }
+}
+;
+
+if (isset($_POST['editarCliente'])) {
+    if (
+        strlen($_POST['client_name']) > 0 &&
+        strlen($_POST['client_email']) > 0 &&
+        strlen($_POST['pwd']) > 0 &&
+        strlen($_POST['client_phone']) > 0 &&
+        strlen($_POST['client_subscription']) > 0 &&
+        strlen($_POST['client_level']) > 0
+    ) {
+        $client_name = $_POST['client_name'];
+        $client_email = $_POST['client_email'];
+        $pwd = $_POST['pwd'];
+        $client_phone = $_POST['client_phone'];
+        $client_subscription = $_POST['client_subscription'];
+        $client_level = $_POST['client_level'];
+        $id = $_POST['id'];
+
+        $clientController = new ClientController();
+        $clientController->editarCliente($client_name, $client_email, $pwd, $client_phone, $client_subscription, $client_level, $id);
+    } else {
+        echo "No se permiten campos vacios.";
+    }
 }
 ;
 
 class ClientController
 {
-    public function crearCliente($client_name, $client_email, $client_phone, $client_subscription, $client_level)
+    public function crearCliente($client_name, $client_email, $pwd, $client_phone, $client_subscription, $client_level)
     {
+        $curl = curl_init();
 
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('name' => $client_name, 'email' => $client_email, 'password' => $pwd, 'phone_number' => $client_phone, 'is_suscribed' => $client_subscription, 'level_id' => $client_level),
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer " . $_SESSION['data']->token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $response = json_decode($response);
+
+        curl_close($curl);
+
+        if (isset($response->message) && $response->code === 4) {
+            header("Location: clientlist.php");
+        } else {
+            echo "Error al crear cliente";
+        }
+    }
+
+    public function editarCliente($client_name, $client_email, $pwd, $client_phone, $client_subscription, $client_level, $id)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS => "name=$client_name&email=$client_email&password=$pwd&phone_number=$client_phone&is_suscribed=$client_subscription&level_id=$client_level&id=$id",
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded',
+                "Authorization: Bearer " . $_SESSION['data']->token
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $response = json_decode($response);
+
+        curl_close($curl);
+
+        if (isset($response->message) && $response->code === 4) {
+            header("Location: clientlist.php");
+        } else {
+            echo "Error al editar cliente";
+        }
     }
 
     public function eliminarCliente($id)
@@ -87,19 +181,19 @@ class ClientController
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-          CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients/'.$id,
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'GET',
-          CURLOPT_HTTPHEADER => array(
-            "Authorization: Bearer " . $_SESSION['data']->token
-          ),
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients/' . $id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer " . $_SESSION['data']->token
+            ),
         ));
-        
+
         $response = curl_exec($curl);
         $response = json_decode($response);
 
@@ -110,7 +204,7 @@ class ClientController
         } else {
             echo "Error al obtener detalles";
         }
-       
+
     }
 
 }
